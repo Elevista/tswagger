4b17cf1bd626bab3614aa598cf6ab1f1190bce60
Elevista <sunnyholic@sunnyholic.com>
2024-07-10 12:54:56 +0900

    fix: Update parametersToTuples to use escapeProp for entry keys

--- src/parametersToTuples.ts
+++ src/parametersToTuples.ts
@@ -1,16 +1,10 @@
 import { Parameter as ParameterV2 } from './spec/v2'
 import { Parameter as ParameterV3 } from './spec/v3'
-import { toValidName } from './utils'
+import { escapeProp, toValidName } from './utils'
 import { schemaToType } from './schemaToType'
 export type Parameter = ParameterV2 | ParameterV3
 
 export type TupleInfo = {
-  /** The original name of the parameter. */
-  name: string
-  /** The typescript type of the parameter. */
-  type: string
-  /** The valid javascript variable name */
-  validName: string
   /**
    * A object entry code
    * @example `originalName: validName`
@@ -27,14 +21,14 @@ export const parameterToTuple = (x: ParameterV2 | ParameterV3): TupleInfo => {
   const schema = 'type' in x ? x : x.schema
   const validName = toValidName(x.name)
   const [name, label] = [x.name, `${validName}${(x.required || x.in === 'path') ? '' : '?'}`]
-  const entry = name === validName ? name : `${name}: ${validName}`
+  const entry = name === validName ? name : `${escapeProp(name)}: ${validName}`
   const type = schema
     ? 'type' in schema && schema.type === 'file'
       ? 'File'
       : schemaToType(schema, false, '')
     : 'unknown'
 
-  return { name, type, validName, entry, tuple: `${label}: ${type}` }
+  return { entry, tuple: `${label}: ${type}` }
 }
 
 export const parametersToTuples = (parameters: Parameter[]) => {

ccdaa54371cec8a992fbf4a6ca3389e4aa021dd6
Elevista <sunnyholic@sunnyholic.com>
2024-07-10 13:25:33 +0900

    refactor: Simplify tsDoc function and improve code readability

--- src/tsDoc.ts
+++ src/tsDoc.ts
@@ -1,5 +1,4 @@
 import { Schema, isReference } from './spec/schema'
-import { isPresent } from './utils'
 
 export const tsDocForm = (content?: string) => {
   if (!content) return ''
@@ -11,10 +10,10 @@ export const tsDocForm = (content?: string) => {
 
 export type DocProps = { summary?: string, description?: string, example?: unknown, default?: unknown } & object
 export const tsDoc = ({ summary, description, example, default: defaults }: DocProps = {}) => tsDocForm([
-  description,
-  summary && description ? `@summary ${summary}` : summary,
+  description || summary,
+  description && summary && `@summary ${summary}`,
   example !== undefined && `@example ${typeof example === 'string' ? example : JSON.stringify(example, null, '  ')}`,
   defaults !== undefined && `@default ${typeof defaults === 'string' ? defaults : JSON.stringify(defaults, null, '  ')}`,
-].filter(isPresent).join('\n'))
+].filter(x => x).join('\n'))
 
 export const docSchema = (schema: Schema) => isReference(schema) ? '' : tsDoc(schema as {})

a9688152ee6e1fc6b17d2b943985a636ccfdd573
Elevista <sunnyholic@sunnyholic.com>
2025-02-10 13:38:42 +0900

    feat: Add support for YAML file parsing in fetchSpec function

--- src/fetchSpec.ts
+++ src/fetchSpec.ts
@@ -4,11 +4,16 @@ import { Swagger } from './spec/v2'
 import fs from 'fs'
 import p from 'path'
 import c from 'chalk'
+import * as YAML from 'yaml'
 export default async function (path: string): Promise<OpenAPI | Swagger> {
   const isRemote = /^[a-z]+?:\/\//.test(path)
-  if (!isRemote) return JSON.parse(fs.readFileSync(p.resolve(path)).toString())
-  console.log(c.cyan(' ℹ fetching'), 'JSON from', c.underline(path))
+  const isYaml = /\.ya?ml$/.test(path)
+  if (!isRemote) {
+    const file = fs.readFileSync(p.resolve(path)).toString()
+    return isYaml ? YAML.parse(file) : JSON.parse(file)
+  }
+  console.log(c.cyan(' ℹ fetching'), `${isYaml ? 'YAML' : 'JSON'} from`, c.underline(path))
   const res = await fetch(path)
   if (res.status >= 400) throw new Error('Fetch Error')
-  return await res.json()
+  return isYaml ? YAML.parse(await res.text()) : res.json()
 }
